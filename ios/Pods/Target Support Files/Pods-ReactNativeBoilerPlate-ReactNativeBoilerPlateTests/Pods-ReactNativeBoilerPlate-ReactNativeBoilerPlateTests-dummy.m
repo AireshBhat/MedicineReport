27011dbf669a597b0dff026013cea3781f3c3675
Airesh Bhat <nelliairesh@gmail.com>
2024-10-20 16:17:19 +0530

    Initial commit
    Generated by react-native@0.73.1

---
+++ ios/Pods/Target Support Files/Pods-ReactNativeBoilerPlate-ReactNativeBoilerPlateTests/Pods-ReactNativeBoilerPlate-ReactNativeBoilerPlateTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ReactNativeBoilerPlate_ReactNativeBoilerPlateTests : NSObject
+@end
+@implementation PodsDummy_Pods_ReactNativeBoilerPlate_ReactNativeBoilerPlateTests
+@end
